7c37852dbe6af8bf1e3d8457bbaa95bcb6206bb5
DerekHalloranDesigns <92124529+DerekHalloranDesigns@users.noreply.github.com>
2021-10-31 13:43:53 -0400

    JQuery Setup + Mobile Menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-console.log("Hey! Welcome to my Portfolio Website!")
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
