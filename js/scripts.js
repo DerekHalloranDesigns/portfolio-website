b7968fee7caafb5caa651571aafeee941ed58833
DerekHalloranDesigns <92124529+DerekHalloranDesigns@users.noreply.github.com>
2022-02-28 00:42:20 -0500

    Home Page Update

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,18 @@
 function menuToggle() {
   var x = document.getElementById("myNavtoggle");
   if (x.className === "navtoggle") {
-      x.className += " responsive";
+     // x.className += " responsive";
+      x.classList.toggle("responsive");
+  } else {
+      x.className = "navtoggle";
+  }
+}
+
+function menuClose() {
+  var x = document.getElementById("myNavtoggle");
+  if (x.className === "navtoggle") {
+     // x.className += " responsive";
+      x.classList.remove("responsive");
   } else {
       x.className = "navtoggle";
   }
